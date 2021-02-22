d6aba4df22d2005cbbad5a465f6a59fa4c6f0ee3
sap98fcs <70250962+sap98fcs@users.noreply.github.com>
2021-02-22 18:15:11 -0500

    Update Treedemonostration2.R

--- Treedemonostration2.R
+++ Treedemonostration2.R
@@ -12,11 +12,10 @@
 # The tutorial will cover the following topics:
 # 1) Taking a look at the data set 
 # 2) An simple example of binary classification
-# 3) Training a tree model for full classification and testing model performance with cross validation
-# 4) Tuning the tree model
-# 5) Cross validation of the tuned model 
-# 6) Improving the performance of decision tree by the random forest approach
-# 7) Making out-of-sample predictions and compare the normal and random forest model
+# 3) Tuning the tree model
+# 4) Cross validation of the tuned model 
+# 5) Improving the performance of decision tree by the random forest approach
+# 6) Making out-of-sample predictions
 
 ### THE DATA ###
 # Data are from the my personal access(as part of the research team) to the ANTIELAB Research Data Archive at HKU (https://antielabdata.jmsc.hku.hk)
@@ -27,17 +26,15 @@
 ### VARIABLES I USE TODAY ###
 # Type - Type of the poster - Solidarity, Mobilize, Police, HKGovt, China
 # The rest are a term-document matrix of columns of dummy variables indicating whether a word appear 
-
+# This tutorial will be collapsed into binary classification, for full classification, visit https://raw.githubusercontent.com/sap98fcs/DecisionTree/main/Treedemonostration2.R
 
 ##### 1) Taking a look at the data set 
 
-#install  and load the necessary packages
+#install and load the necessary packages
 install.packages("mlr")
 install.packages("tidyverse")
-install.packages("randomForest")
 library(mlr)
 library(tidyverse)
-library(randomForest)
 
 #Import data from github
 data <- read.csv("https://raw.githubusercontent.com/sap98fcs/DecisionTree/main/DecisionTree.csv")
@@ -53,44 +50,14 @@ data %>%
   group_by(Type) %>%
   summarise(no_rows = length(Type))
 
-#See the predictors
-colnames(data[,2:ncol(data)])
-
 #Summary on number of predictors
 count(data[2:63],as.factor(rowSums(data[2:63])))
 
 # 2) An simple example of binary classification
 
-#modify the dataset
-data_bin <- data
-data_bin$Type[data_bin$Type != "Mobilize" ] <- "nonMobilize" 
-
-#set the task, learner, and train the model
-simplePosterTask <- makeClassifTask(data = data_bin, target = "Type")
-tree <- makeLearner("classif.rpart",predict.type = "prob")
-simplemodel <- train(tree, simplePosterTask)
-
-#cross validation of the naive test
-CV <- makeResampleDesc("CV", iters = 5)
-simplemodel_CV <- resample(tree, simplePosterTask,
-                          resampling = CV,
-                          measures = acc)
-#view the tree
-treeModelData <- getLearnerModel(simplemodel)
-install.packages("rpart.plot")
-library(rpart.plot)
-rpart.plot(treeModelData, roundint = FALSE,
-           box.palette = "BuBn",
-           type =5)
-
-rm(data_bin)
-rm(simplemodel_CV)
-rm(simplePosterTask)
-rm(simplemodel)
-rm(treeModelData)
-
-# 3) Training a tree model for full classification and testing model performance with cross validation
-#separate test set and training set
+#modify the data set for binary classification 
+data$Type[data$Type != "Mobilize" ] <- "nonMobilize"
+data$Type <- as.character(data$Type)
 
 #make sure we have the same random number
 set.seed(1234)
@@ -100,7 +67,6 @@ train_rows <- sample(seq_len(nrow(data)), nrow(data)*3/4)
 train_data <- data[train_rows, ]
 test_data <- data[-train_rows, ]
 rm(train_rows)
-rm(data)
 
 #Summary on types of poster of the two sets
 train_data %>% 
@@ -114,29 +80,31 @@ test_data %>%
 count(train_data[2:63],as.factor(rowSums(train_data[2:63])))
 count(test_data[2:63],as.factor(rowSums(test_data[2:63])))
 
-
-#setting up test and do a naive test
+#set the task, learner, and train the model
+tree <- makeLearner("classif.rpart",predict.type = "prob")
 PosterTask <- makeClassifTask(data = train_data, target = "Type")
 naivemodel <- train(tree, PosterTask)
 
 #cross validation of the naive test
 CV <- makeResampleDesc("CV", iters = 5)
 naivemodel_CV <- resample(tree, PosterTask,
-                     resampling = CV,
-                     measures = acc)
-#It performs worse compare to the previous model for binary classification 
+                          resampling = CV,
+                          measures = acc)
+
+naivemodel_CV$aggr
 
 #view the tree
 treeModelData <- getLearnerModel(naivemodel)
+install.packages("rpart.plot")
+library(rpart.plot)
 rpart.plot(treeModelData, roundint = FALSE,
            box.palette = "BuBn",
            type =5)
 
-#It also wasted a lot of variables!
-rm(naivemodel)
 rm(treeModelData)
+rm(naivemodel)
 
-# 4) Tuning the tree model with cross validation 
+# 3) Tuning the tree model with cross validation 
 #The hyperparameters of a decision tree, see figure 7.7 in Rhys(2020)
 
 getParamSet(tree)
@@ -159,7 +127,7 @@ library(parallel)
 library(parallelMap)
 
 #Set the random search parameter
-randSearch <- makeTuneControlRandom(maxit = 200)
+randSearch <- makeTuneControlRandom(maxit = 100)
 
 #Performing hyperparameters tuning
 
@@ -172,13 +140,12 @@ tunedTreePars <- tuneParams(tree, task = PosterTask,
 
 parallelStop()
 
+#see the tuned parameters
+tunedTreePars$x
+
 #compare the performance of the tuning process with the naive model
 tunedTreePars$y
 naivemodel_CV$aggr
-rm(naivemodel_CV)
-
-#see the tuned parameters
-tunedTreePars$x
 
 #get the tuned model 
 tunedTree <- setHyperPars(tree, par.vals = tunedTreePars$x)
@@ -192,10 +159,9 @@ rpart.plot(treeModelData, roundint = FALSE,
 
 rm(treeModelData)
 rm(tunedTree)
+rm(naivemodel_CV)
 
-#Download the pdf and see what have happened!
-
-# 5) Cross validation of the tuned model 
+# 4) Cross validation of the tuned model 
 
 #wrap the learner with the tuning process)
 treeWrapper <- makeTuneWrapper(tree, resampling = CV,
@@ -210,8 +176,8 @@ cvWithTuning <- resample(treeWrapper, PosterTask, resampling = CV)
 
 parallelStop()
 
-#The nested CV's accuracy is similar to that of the Tuning process, meaning that the model is a good fit!
-cvWithTuning
+#Compare the nested CV's accuracy with the Tuning process
+cvWithTuning$aggr
 tunedTreePars$y
 
 rm(tunedTreePars)
@@ -219,26 +185,31 @@ rm(treeWrapper)
 rm(treeParamSpace)
 rm(tree)
 
-# 6) Improve the performance of decision tree by the random forest approach
+# 5) Improve the performance of decision tree by the random forest approach
 
 #The basic idea to create multiple model by bootstrapping, and then each model cast a vote on the prediction 
 #Bootstrapping could reduce variance (avoiding overfitting) as outliners have less chance to be selected
 #In each node, the number of features to be considered is randomly selected to increase the independency between model
 
+#install the RF packet
+install.packages("randomForest")
+library(randomForest)
+
 #define the new learner
 forest <- makeLearner("classif.randomForest", predict.type = "prob")
 
-#set hyperparameters of random forest learner
-#ntree！ The number of trees in the forest
-#mtry！ The number of features to randomly sample at each node
-#nodesize！ The minimum number of cases after a split (the same as minbucket in the tree)
-#maxnodes！ The maximum number of leaves allowed
+getParamSet(forest)
+#ntree - The number of trees in the forest
+#mtry - The number of features to randomly sample at each node
+#nodesize - The minimum number of cases after a split (the same as minbucket in the tree)
+#maxnodes - The maximum number of leaves allowed
 
+#set hyperparameters of random forest learner
 forestParamSpace <- makeParamSet(                        
-   makeIntegerParam("ntree", lower = 100, upper = 300),
-   makeIntegerParam("mtry", lower = 15, upper = 30),
-   makeIntegerParam("nodesize", lower = 1, upper = 5),
-   makeIntegerParam("maxnodes", lower = 5, upper = 20))
+  makeIntegerParam("ntree", lower = 100, upper = 200),
+  makeIntegerParam("mtry", lower = 15, upper = 30),
+  makeIntegerParam("nodesize", lower = 1, upper = 5),
+  makeIntegerParam("maxnodes", lower = 5, upper = 20))
 
 parallelStartSocket(cpus = detectCores())
 
@@ -269,9 +240,9 @@ cvWithTuning <- resample(forestWrapper, PosterTask, resampling = CV)
 
 parallelStop()
 
-#The nested CV's accuracy a little bit lower than that of the tuning process, meaning that the model is a little bit over-fitted!
-cvWithTuning
-tunedTreePars$y
+#Compare the nested CV's accuracy with the Tuning process
+tunedForestPars$y
+cvWithTuning$aggr
 
 rm(forestParamSpace)
 rm(randSearch)
@@ -280,11 +251,10 @@ rm(train_data)
 rm(forest)
 rm(CV)
 rm(cvWithTuning)
-rm(forestParamSpace)
 rm(forestWrapper)
 rm(PosterTask)
 
-# 7) Making out-of-sample predictions 
+# 6) making out-of-sample predictions 
 
 #accuracy of the tree model
 prediction_tree <- as.tibble(predict(tunedTreeModel, newdata = test_data))
@@ -315,4 +285,95 @@ prediction_forest <- prediction_forest %>%
 acc_forest <- sum(prediction_forest$correct)/nrow(prediction_forest)
 acc_forest
 
-#The accuracy is the same because posters for Mobilize and Solidarity share many similar feature.
+#get feature importance in a model
+t <- getFeatureImportance(tunedForestModel)
+t <- t$res[order(t$res[, "importance"], decreasing = TRUE),]
+
+z <- getFeatureImportance(tunedTreeModel)
+z <- z$res[order(z$res[, "importance"], decreasing = TRUE),]
+
+install.packages("dplyr")
+library(dplyr)
+featureimportance <- inner_join(t,z, by="variable")
+
+#Recode the data into 1(Mobilize as positive) and 0(nonMobilize as negative)
+
+prediction_tree$truth <- as.character(prediction_tree$truth)
+prediction_tree$response <- as.character(prediction_tree$response)
+prediction_forest$truth <- as.character(prediction_forest$truth)
+prediction_forest$response <- as.character(prediction_forest$response)
+
+prediction_tree$truth[prediction_tree$truth == "nonMobilize"] <- 0
+prediction_tree$truth[prediction_tree$truth == "Mobilize"] <- 1
+prediction_tree$response[prediction_tree$response == "nonMobilize"] <- 0
+prediction_tree$response[prediction_tree$response == "Mobilize"] <- 1
+
+prediction_forest$truth[prediction_forest$truth == "nonMobilize"] <- 0
+prediction_forest$truth[prediction_forest$truth == "Mobilize"] <- 1
+prediction_forest$response[prediction_forest$response == "nonMobilize"] <- 0
+prediction_forest$response[prediction_forest$response == "Mobilize"] <- 1
+
+
+#False Positive Rate
+#first, create a variable that identifies everything that was true 0, but classified as 1
+prediction_tree <- prediction_tree %>%
+  mutate(fp = case_when(
+    (truth == 0 & response == 1) ~ 1,
+    (truth == 1 | response == 0) ~ 0
+  ))
+
+#same for random forest 
+prediction_forest <- prediction_forest %>%
+  mutate(fp = case_when(
+    (truth == 0 & response == 1) ~ 1,
+    (truth == 1 | response == 0) ~ 0
+  ))
+
+#False Positive Rate = FP/All Negative
+fpr_tree <- sum(prediction_tree$fp)/(nrow(prediction_tree)-sum(as.numeric(prediction_tree$truth)))
+fpr_forest <- sum(prediction_forest$fp)/(nrow(prediction_forest)-sum(as.numeric(prediction_forest$truth)))
+
+fpr_tree
+fpr_forest 
+
+
+#True Positive Rate (Recall) and Precision 
+
+#first, create a variable that identifies everything that was true 1, and also classified as 1
+prediction_tree <- prediction_tree %>%
+  mutate(tp = case_when(
+    (truth == 1 & response == 1) ~ 1,
+    (truth == 0 | response == 0) ~ 0
+  ))
+
+prediction_forest <- prediction_forest %>%
+  mutate(tp = case_when(
+    (truth == 1 & response == 1) ~ 1,
+    (truth == 0 | response == 0) ~ 0
+  ))
+
+#True Positive Rate/Recall = TP/All Positive
+tpr_tree <- sum(prediction_tree$tp)/sum(as.numeric(prediction_tree$truth))
+tpr_forest <- sum(prediction_forest$tp)/sum(as.numeric(prediction_forest$truth))
+
+tpr_tree
+tpr_forest
+
+#Precision = TP/TP+FP
+pre_tree <- sum(prediction_tree$tp)/(sum(prediction_tree$tp)+sum(prediction_tree$fp))
+pre_forest <- sum(prediction_forest$tp)/(sum(prediction_forest$tp)+sum(prediction_forest$fp))
+
+pre_tree
+pre_forest
+
+#install package for AUC PR Curve
+install.packages("MLmetrics")
+library(MLmetrics)
+
+# Area under ROC curve
+AUC(y_pred = prediction_tree$prob.Mobilize,y_true=prediction_tree$truth)
+AUC(y_pred = prediction_forest$prob.Mobilize,y_true=prediction_forest$truth)
+
+# Area under precision-recall curve
+PRAUC(y_pred = prediction_tree$prob.Mobilize,y_true=prediction_tree$truth)
+PRAUC(y_pred = prediction_forest$prob.Mobilize,y_true=prediction_forest$truth)
